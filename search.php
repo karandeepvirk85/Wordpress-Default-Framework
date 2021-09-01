15a72ba4b2df9316616e083e0ea9115f957a6345
karandeepvirk85 <karandeep.virk@yahoo.com>
2021-09-01 14:57:34 +0530

    removed Search Template

--- search.php
+++ search.php
@@ -4,10 +4,5 @@
  * Theme Name: gTheme
  * Template Author: Karandeep Singh Virk
  */
-get_header();
-if ( have_posts() ) :
-while ( have_posts() ) : the_post();
-    endwhile;
-endif;
-get_footer();
+
 ?>
