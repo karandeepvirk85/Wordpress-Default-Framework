78bb6cf9b6135ee84ba685acb1f9d5139f50217b
karandeepvirk85 <karandeep.virk@yahoo.com>
2021-09-27 12:42:20 +0530

    Added Stripe Payment Form CSS

--- tp-payment.php
+++ tp-payment.php
@@ -15,32 +15,56 @@ if(class_exists('Stripe_Controller')){
     <div class="container stripe-container">
         <form id="payment-stripe" method ="post">
             <div class="row">
+                <!--First Name-->
                 <div class="col-md-5">
-                    <label>First Name</label>
-                    <input type="text" name="first_name" class="form-control">
+                    <label>First Name <span class="stripe-required">*<span></label>
+                    <input type="text" required="required" placeholder="Write First Name" name="first_name" class="form-control input-text">
                 </div>
+                <!--Last Name-->
                 <div class="col-md-5">
-                    <label>Last Name</label>
-                    <input type="text" name="last_name" class="form-control">
+                    <label>Last Name <span class="stripe-required">*<span></label>
+                    <input type="text" required="required" placeholder="Write Last Name" name="last_name" class="form-control input-text">
                 </div>
+                <!--Amount-->
                 <div class="col-md-2">
-                <label>Amount</label>
-                    <input type="number" name="stripeAmount" class="form-control">
+                <label>Amount<span class="stripe-required">*<span></label>
+                    <input type="number" required="required" placeholder="Amount" name="amount" class="form-control input-text">
                 </div>
             </div>
 
+            <p class="spacer"></p>
+            
+            <div class="row">
+                <!--Email-->
+                <div class="col-md-6">
+                    <label>Email<span class="stripe-required">*<span></label>
+                    <input type="email"  placeholder="Your email to get receipt" name="email" class="form-control input-text">
+                </div>
+                <!--Phone-->
+                <div class="col-md-6">
+                    <label>Phone</label>
+                    <input type="tel" name="phone" placeholder="Your Phone Number" class="form-control input-text">
+                </div>
+            </div>
+
+            <p class="spacer"></p>
+            
             <div class="row">
                 <div class="col-md-12">
+                    <!--Stripe Card Element-->
                     <div id="card-element"></div>
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <div id="card-errors"></div>
+            <!--Submit Button and Errors-->
+            <div class="row">
+                <div class="col-md-12">
+                    <button type="submit" class="btn-block btn btn-primary btn-theme">SUBMIT</button>
+                    <p class="spacer"></p>
+                    <div id="card-errors"></div>
+                </div>
+            </div>
         </form>
     </div>
 </div>
-<script>
-   
-</script>
 <?php get_footer();?>
